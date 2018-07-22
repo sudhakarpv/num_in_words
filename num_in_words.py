b6efc640cfca8bc7aa09b25d084bf353a76bbc16
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-22 12:40:51 +0530

    Update and rename README.md to num_in_words.py

---
+++ num_in_words.py
@@ -0,0 +1,10 @@
+# num_in_words
+def main():
+    pass
+    n=input()
+    num={"1":"One","2":"Two","3":"Three","4":"Four","5":"Five","6":"Six","7":"Seven",
+         "8":"Eight","9":"Nine","10":"Ten"}
+    print(num[n])
+
+if __name__ == '__main__':
+    main()
